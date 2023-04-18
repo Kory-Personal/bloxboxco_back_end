fb5bb6968d421dcc1a76cd5ded4276e3dbd1f6a5
Kory Jackson <kory.a.jaxn@gmail.com>
2023-04-18 11:17:03 -0700

    created and connected mongodb, server runs on 3001

---
+++ README copy.md
@@ -0,0 +1,34 @@
+# LAB - Authenticated API Server
+
+## Authenticated API
+
+### Author: Kory Jackson
+
+
+### Setup
+
+- [live server](https://authenticatedapiserver.herokuapp.com/)
+
+#### `.env` requirements
+
+- `PORT` - 3000
+
+#### Running the app
+
+- `npm start`
+- in terminal `http post :3000/signup username=ENTERHERE password=ENTERHERE role=(guest, author, editor, admin)`
+  - Returns User Object with token
+- endpoint `post :3000/signin -a username:password`
+  - returns user token
+- endpoint ``
+
+
+#### Tests
+
+- Unit Tests: `npm test`
+- tests report ![test results](./assets/test_coverage_authenticated_api.png)
+
+
+#### UML
+
+- None available
---
+++ server.js
@@ -0,0 +1,42 @@
+"use strict";
+
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+// dependencies
+const logger = require("./middleware/logger.js");
+const apiRoutes = require("./api/api-v1.js");
+const authRoutes = require("./auth/authRoutes.js");
+const notFoundHandler = require("./middleware/404.js");
+const errorHandler = require("./middleware/500.js");
+
+const app = express();
+
+//Global Variables
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(logger);
+app.get("/", (req, res) => {
+  let obj = {
+    categories: "/api/v1/categories",
+    products: "/api/v1/products",
+  }
+  res.status(200).send("Are you looking for some droids?? For Categories: /api/v1/categories, for Products: /api/v1/products and for Todo: /api/v1/todo")
+})
+
+
+//Routes
+app.use("/api/v1/", apiRoutes);
+app.use(authRoutes);
+
+app.use("*", notFoundHandler);
+app.use(errorHandler);
+
+module.exports = {
+  app,
+  start: (port) => app.listen(port, console.log("Up on ", port)),
+};
